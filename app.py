75016520214479aff479ef1134e7b2f3b8c617b6
vishrutss <vishrutss@gmail.com>
2023-10-23 15:20:20 -0700

    Added code for text detection in the uploaded image. Also updated gitignore.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template
 import os
+import pytesseract
+import cv2
 
 app = Flask(__name__)
 
-# Creating a folder for storing files to be uploaded
+pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_CMD', 'tesseract')
 UPLOAD_FOLDER = 'images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Make sure the allowed extensions are png and jpg
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_types(filename):
@@ -24,10 +25,19 @@ def upload_images():
             return "No file selected"
         
         if allowed_types(file.filename):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            return "File uploaded successfully"
-        
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(image_path)
+            text = read_image(image_path)
+            return render_template('result.html', image_path=image_path, extracted_text=text)
+
     return render_template('index.html')
+
+def read_image(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    text = pytesseract.image_to_string(threshold_img)
+    return text
 
 if __name__ == '__main__':
     app.run(debug=True)
