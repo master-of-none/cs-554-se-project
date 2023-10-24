890117f880b0a68cabdb81393e7a0127989ad169
vishrutss <vishrutss@gmail.com>
2023-10-24 10:09:25 -0700

    Removed pytesseract and implemented google vision api instead for text detection.

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, request, render_template
 import os
-import pytesseract
-import cv2
+from typing import Sequence
+from google.cloud import vision
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'google-cloud-vision-credentials.json'
 
 app = Flask(__name__)
 
-pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_CMD', 'tesseract')
 UPLOAD_FOLDER = 'images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def analyze_image_from_uri(
+        image_uri: str,
+        feature_types: Sequence,
+) -> vision.AnnotateImageResponse:
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.Image()
+    image.source.image_uri = image_uri
+    features = [vision.Feature(type_=feature_type) for feature_type in feature_types]
+    rq = vision.AnnotateImageRequest(image=image, features=features)
+
+    response = client.annotate_image(request=rq)
+
+    return response
 
 def allowed_types(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -19,25 +35,29 @@ def upload_images():
     if request.method == 'POST':
         if 'file' not in request.files:
             return "No file found"
-        
+
         file = request.files['file']
         if file.filename == '':
             return "No file selected"
-        
+
         if allowed_types(file.filename):
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(image_path)
-            text = read_image(image_path)
+            features = [vision.Feature.Type.TEXT_DETECTION]
+            response = analyze_image_from_uri(image_path, features)
+            text = detect_text(response)
             return render_template('result.html', image_path=image_path, extracted_text=text)
 
     return render_template('index.html')
 
-def read_image(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    text = pytesseract.image_to_string(threshold_img)
-    return text
+def detect_text(response: vision.AnnotateImageResponse):
+    print("=" * 80)
+    string = ""
+    for annotation in response.text_annotations:
+        vertices = [f"({v.x},{v.y})" for v in annotation.bounding_poly.vertices]
+        string.join(f"{repr(annotation.description):42} | {', '.join(vertices)}\n")
+    return string
+
 
 if __name__ == '__main__':
     app.run(debug=True)
