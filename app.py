992884aa0bbf47ab08f5ab58d9823cf01370c2ef
vishrutss <vishrutss@gmail.com>
2023-10-24 11:45:53 -0700

    Removed analyze_image_from_uri and implemented detect text in a single function. Also fixed text detection error due to image path format issue.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import io
+import pandas as pd
 from flask import Flask, request, render_template
 import os
-from typing import Sequence
-from google.cloud import vision
+from google.cloud import vision, vision_v1
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'google-cloud-vision-credentials.json'
 
@@ -12,20 +13,24 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-def analyze_image_from_uri(
-        image_uri: str,
-        feature_types: Sequence,
-) -> vision.AnnotateImageResponse:
+def detect_text(img):
     client = vision.ImageAnnotatorClient()
+    with io.open(img, 'rb') as image_file:
+        content = image_file.read()
+    image = vision_v1.types.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    df = pd.DataFrame()
 
-    image = vision.Image()
-    image.source.image_uri = image_uri
-    features = [vision.Feature(type_=feature_type) for feature_type in feature_types]
-    rq = vision.AnnotateImageRequest(image=image, features=features)
+    for text in texts:
+        new_data = pd.DataFrame({
+            'locale': [text.locale],
+            'description': [text.description]
+        })
+        df = pd.concat([df, new_data], ignore_index=True)
 
-    response = client.annotate_image(request=rq)
-
-    return response
+    print(df)
+    return df
 
 def allowed_types(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -43,20 +48,10 @@ def upload_images():
         if allowed_types(file.filename):
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(image_path)
-            features = [vision.Feature.Type.TEXT_DETECTION]
-            response = analyze_image_from_uri(image_path, features)
-            text = detect_text(response)
+            text = str(detect_text(image_path))
             return render_template('result.html', image_path=image_path, extracted_text=text)
 
     return render_template('index.html')
-
-def detect_text(response: vision.AnnotateImageResponse):
-    print("=" * 80)
-    string = ""
-    for annotation in response.text_annotations:
-        vertices = [f"({v.x},{v.y})" for v in annotation.bounding_poly.vertices]
-        string.join(f"{repr(annotation.description):42} | {', '.join(vertices)}\n")
-    return string
 
 
 if __name__ == '__main__':
